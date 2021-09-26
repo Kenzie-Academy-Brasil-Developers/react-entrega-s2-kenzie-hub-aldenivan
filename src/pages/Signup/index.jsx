461b60b6a0f7506ef8714683dd038c91a6177020
Aldenivan Jr <aldenivan1207@gmail.com>
2021-09-26 19:24:41 -0300

    Assembling the structure of the signup page

--- src/pages/Signup/index.jsx
+++ src/pages/Signup/index.jsx
@@ -1,5 +1,24 @@
+import Button from "../../components/Button";
+
 const Signup = () => {
-  return <div>Signup</div>;
+  return (
+    <div>
+      <div />
+      <div>
+        <div>
+          <form>
+            <h1>Cadastro</h1>
+            <input></input>
+            <input></input>
+            <input></input>
+            <input></input>
+            <Button>Enviar</Button>
+            <p>Ja tem uma conta? Faça seu login</p>
+          </form>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Signup;
