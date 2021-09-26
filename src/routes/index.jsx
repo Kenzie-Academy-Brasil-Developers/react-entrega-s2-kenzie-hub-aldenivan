35c3e52fb14aad2cda8a67735a97c0eada43fe7e
Aldenivan Jr <aldenivan1207@gmail.com>
2021-09-26 19:08:24 -0300

    Creating the signup component and the start structure, also calling in the routes

--- src/routes/index.jsx
+++ src/routes/index.jsx
@@ -1,11 +1,15 @@
 import { Switch, Route } from "react-router-dom";
 import Home from "../pages/Home";
+import Signup from "../pages/Signup";
 
 const Routes = () => {
   return (
     <Switch>
       <Route exact path="/">
         <Home />
+      </Route>
+      <Route path="/signup">
+        <Signup />
       </Route>
     </Switch>
   );
