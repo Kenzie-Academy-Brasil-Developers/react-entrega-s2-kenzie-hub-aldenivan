02c133b85e6f51877ac31c9db3b1e1ef9ac6878f
Aldenivan Jr <aldenivan1207@gmail.com>
2021-09-26 22:57:46 -0300

    Creating Dashboard component and route calling he

--- src/routes/index.jsx
+++ src/routes/index.jsx
@@ -2,6 +2,7 @@ import { Switch, Route } from "react-router-dom";
 import Home from "../pages/Home";
 import Signup from "../pages/Signup";
 import Login from "../pages/Login";
+import Dashboard from "../pages/Dashboard";
 
 const Routes = () => {
   return (
@@ -14,6 +15,9 @@ const Routes = () => {
       </Route>
       <Route path="/login">
         <Login />
+      </Route>
+      <Route path="/dashboard">
+        <Dashboard />
       </Route>
     </Switch>
   );
